6e5400589b70d5895f731ce3ffa636cd0391d282
Alang Kim <rlaangh77@naver.com>
2021-11-30 01:37:25 +0900

    feat: SM-8 Text Component 추가

---
+++ src/components/base/Text.tsx
@@ -0,0 +1,24 @@
+import type { ReactText } from 'react';
+import styled from '@emotion/styled';
+
+import type { SizeType } from '@/types';
+import { TEXT_SIZE } from '@utils/constants/size';
+
+export interface TextProps {
+  [prop: string]: any;
+  children: ReactText;
+  size?: SizeType;
+  color?: string;
+  bold?: boolean;
+  block?: boolean;
+}
+
+const Text = styled.span<TextProps>`
+  font-size: ${({ size }): string =>
+    size ? `${TEXT_SIZE[size]}px` : `${TEXT_SIZE['md']}px`};
+  color: ${({ color }): string | undefined => color};
+  font-weight: ${({ bold }): string | boolean | undefined => bold && '600'};
+  display: ${({ block }): string => (block ? 'block' : 'inline')};
+`;
+
+export default Text;
