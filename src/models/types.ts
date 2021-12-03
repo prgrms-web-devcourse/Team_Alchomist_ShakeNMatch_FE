e6400d0795b7e85ae8bf7e96b9e2dfd5b4a97c25
42KIM <ksahoo524@gmail.com>
2021-12-03 18:26:20 +0900

    feat: SM-62 Color 상수화 및 타입 정의

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,3 +1,8 @@
+import type { COLOR } from '@constants/colors';
+
+type ColorKeys = keyof typeof COLOR;
+type ColorType = typeof COLOR[ColorKeys];
+
 type SizeType = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-export type { SizeType };
+export type { ColorType, SizeType };
