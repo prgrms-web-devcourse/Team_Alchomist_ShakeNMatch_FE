3469295e7156e28eb7721e450e6db409ae3dd064
42KIM <ksahoo524@gmail.com>
2021-12-05 05:24:37 +0900

    feat: SM-52 TextButton 컴포넌트 초기 구현

--- src/components/compound/TextButton/index.tsx
+++ src/components/compound/TextButton/index.tsx
@@ -1,22 +1,76 @@
 import { Button, Text } from '@base';
 import type { ReactElement } from 'react';
-import type { ButtonProps } from '@base/Button/types';
-import type { TextProps } from '@base/Text/types';
+import { COLOR } from '@constants/colors';
+
+const TEXT_BUTTON = {
+  SHORT_WHITE: {
+    buttonProps: {
+      type: 'button',
+      size: 'short',
+      basicColor: 'BASIC_WHITE',
+      hoverColor: 'LIGHT_PINK',
+      clickedColor: 'BASIC_PINK'
+    },
+    textProps: {
+      size: 'sm',
+      color: COLOR.BLACK
+    }
+  },
+  SHORT_PINK: {
+    buttonProps: {
+      type: 'button',
+      size: 'short',
+      basicColor: 'BASIC_PINK',
+      hoverColor: 'MIDDLE_PINK',
+      clickedColor: 'STRONG_PINK'
+    },
+    textProps: {
+      size: 'sm',
+      color: COLOR.BASIC_WHITE
+    }
+  },
+  LONG_WHITE: {
+    buttonProps: {
+      type: 'button',
+      size: 'long',
+      basicColor: 'BASIC_WHITE',
+      hoverColor: 'LIGHT_PINK',
+      clickedColor: 'BASIC_PINK'
+    },
+    textProps: {
+      size: 'md',
+      color: COLOR.BLACK
+    }
+  },
+  LONG_PINK: {
+    buttonProps: {
+      type: 'button',
+      size: 'long',
+      basicColor: 'BASIC_PINK',
+      hoverColor: 'MIDDLE_PINK',
+      clickedColor: 'STRONG_PINK'
+    },
+    textProps: {
+      size: 'md',
+      color: COLOR.BASIC_WHITE
+    }
+  }
+} as const;
+
+type TextButtonKeys = keyof typeof TEXT_BUTTON;
 
 export interface TextButtonProps {
   children: string;
-  buttonProps: ButtonProps;
-  textProps: TextProps;
+  buttonType: TextButtonKeys;
 }
 
 const TextButton = ({
   children,
-  buttonProps,
-  textProps
+  buttonType = 'SHORT_WHITE'
 }: TextButtonProps): ReactElement => {
   return (
-    <Button {...buttonProps}>
-      <Text {...textProps}>{children}</Text>
+    <Button {...TEXT_BUTTON[buttonType].buttonProps}>
+      <Text {...TEXT_BUTTON[buttonType].textProps}>{children}</Text>
     </Button>
   );
 };
--- src/stories/compound/TextButton.stories.tsx
+++ src/stories/compound/TextButton.stories.tsx
@@ -1,26 +1,18 @@
+import type { TextButtonProps } from '@components/compound/TextButton';
 import TextButton from '@components/compound/TextButton';
-// import type { TextButtonProps } from 'components/compound/TextButton';
 import type { ReactElement } from 'react';
 
 export default {
   title: 'Component/TextButton',
-  component: TextButton
+  component: TextButton,
+  argTypes: {
+    buttonType: {
+      control: 'inline-radio',
+      oprtions: ['SHORT_WHITE', 'SHORT_PINK', 'LONG_WHITE', 'LONG_PINK']
+    }
+  }
 };
 
-export const Default = (): ReactElement => {
-  return (
-    <TextButton
-      buttonProps={{
-        type: 'button',
-        size: 'sm',
-        color: 'primary'
-      }}
-      textProps={{
-        size: 'lg',
-        color: 'red'
-      }}
-    >
-      하이
-    </TextButton>
-  );
+export const Default = (props: TextButtonProps): ReactElement => {
+  return <TextButton {...props}>하이</TextButton>;
 };
