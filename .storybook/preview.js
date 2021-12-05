9a36bdc4eaead66baf21d4f7ea168da31e9980dd
Alang Kim <rlaangh77@naver.com>
2021-12-06 01:02:24 +0900

    feat: SM-64 reset-css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,36 @@
+import { css, Global } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Global
+        styles={css`
+          ${emotionNormalize}
+          *, *::after, *::before {
+            box-sizing: border-box;
+          }
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            background: white;
+            min-height: 100%;
+            font-family: Helvetica, Arial, sans-serif;
+          }
+        `}
+      />
+      <Story />
+    </>
+  )
+];
