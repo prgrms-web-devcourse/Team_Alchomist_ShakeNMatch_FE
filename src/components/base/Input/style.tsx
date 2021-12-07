86f5d1671d0cbb5d67e3d69cefe95ad5bcf73cfa
Alang Kim <rlaangh77@naver.com>
2021-12-07 18:01:29 +0900

    refactor: SM-72 Input Background 변경

--- src/components/base/Input/style.tsx
+++ src/components/base/Input/style.tsx
@@ -1,16 +1,15 @@
 import styled from '@emotion/styled';
 import type { InputProps } from './types';
-
-import { TEXT_SIZE } from '@utils/constants';
+import { COLOR, TEXT_SIZE } from '@utils/constants';
 
 const StyledInput = styled.input<Required<Pick<InputProps, 'fontSize'>>>`
   font-size: ${({ fontSize }): string => TEXT_SIZE[fontSize]};
-  border-radius: 1rem;
   width: 393px;
   height: 52px;
-  background: #eae4e4;
+  background: ${COLOR.IVORY};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 7px;
+  border: none;
 `;
 
 export { StyledInput };
