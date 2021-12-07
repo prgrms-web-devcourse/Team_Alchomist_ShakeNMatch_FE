4ffa259cf6a9f783311e2a1a62c8c032cec7cce6
Alang Kim <rlaangh77@naver.com>
2021-12-07 16:54:07 +0900

    chore: SM-75 Base index export

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -1,2 +1,8 @@
 export { default as Button } from '@base/Button';
 export { default as Text } from '@base/Text';
+export { default as Divider } from '@base/Divider';
+export { default as Icon } from '@base/Icon';
+export { default as Image } from '@base/Image';
+export { default as Input } from '@base/Input';
+export { default as Modal } from '@base/Modal';
+export { default as SectionDivider } from '@base/SectionDivider';
