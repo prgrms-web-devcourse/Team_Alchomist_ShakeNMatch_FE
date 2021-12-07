d75c3f7f8506adf901a6c8e7c2975a12b785500a
Alang Kim <rlaangh77@naver.com>
2021-12-07 17:56:52 +0900

    feat: SM-72 Text Component dangerously 속성 추가
    dangerously prop 부여시, 작성한 textcontent가 innerHTML로 작성됨 ( <br> 와 같은것들 사용가능)

--- src/components/base/Text/index.tsx
+++ src/components/base/Text/index.tsx
@@ -1,3 +1,21 @@
-import { Text } from './styled';
+import type { ReactElement } from 'react';
+import { StyledText } from './styled';
+import type { TextProps } from './types';
+
+const Text = ({
+  children,
+  dangerously = false,
+  ...props
+}: TextProps): ReactElement => {
+  if (dangerously && children) {
+    return (
+      <StyledText
+        dangerouslySetInnerHTML={{ __html: children }}
+        {...props}
+      ></StyledText>
+    );
+  }
+  return <StyledText {...props}>{children}</StyledText>;
+};
 
 export default Text;
