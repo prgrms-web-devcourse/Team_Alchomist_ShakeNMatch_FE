96b6296b68102fc0ea5931ae495bf42ac97bc6dc
42KIM <ksahoo524@gmail.com>
2021-12-08 15:05:03 +0900

    feat: SM-80 style 수정

--- src/components/domain/IngredientToggleList/index.tsx
+++ src/components/domain/IngredientToggleList/index.tsx
@@ -1,19 +1,4 @@
 /* eslint-disable react/jsx-key */
-// 재료 배열을 받는다
-// 배열을 map 하며 TextToggle을 생성한다.
-//   children,   -> 이름
-//   block = false,
-//   name, -> 상태 저장용 이름
-//   initialState = false,
-//   toggleType = 'ingredient',
-//   onChange
-
-// 토글이 될 때마다 onChange 함수가 호출된다.
-// onChange 함수는 해당 버튼의 name을 List의 상태값에 추가한다.
-
-// List는 onChange 함수를 받는다. 그래서 상태값이 바뀔때마다 상위 컴포넌트에 상태를 업데이트 한다.
-// 그런데 div에는 onChange가 업으니까 useEffect를 써서 상태가 바뀔때마다 수동으로 업데이트를 시켜줘야겠네
-
 import { useState, Children, useEffect } from 'react';
 import type { ReactElement } from 'react';
 import TextToggle from '@compound/TextToggle';
--- src/components/domain/IngredientToggleList/styled.ts
+++ src/components/domain/IngredientToggleList/styled.ts
@@ -4,9 +4,10 @@ const StyledContainer = styled.div`
   border: 1px solid;
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
   align-content: flex-start;
-  width: 1000px;
-  height: 1000px;
+  width: 820px;
+  height: 500px;
   gap: 10px;
   padding: 10px;
 `;
