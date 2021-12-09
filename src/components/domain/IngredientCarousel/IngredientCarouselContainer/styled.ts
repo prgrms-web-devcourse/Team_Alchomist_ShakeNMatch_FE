637a7145daefb8d7ea280963e33c37de6eb2e5c1
42KIM <ksahoo524@gmail.com>
2021-12-10 01:25:21 +0900

    feat: SM-71 StyledCarouselContainer width 고정값 추가

--- src/components/domain/IngredientCarousel/IngredientCarouselContainer/styled.ts
+++ src/components/domain/IngredientCarousel/IngredientCarouselContainer/styled.ts
@@ -5,10 +5,10 @@ const StyledCarouselContainer = styled.div<StyledCarouselContainerProps>`
   position: relative;
   display: flex;
   flex-wrap: wrap;
+  width: 630px;
   height: ${({ row }): string | undefined =>
     row === 'single' ? undefined : '300px'};
   justify-content: flex-start;
-  padding: 0 50px;
   gap: 30px;
   padding: 0 90px;
 `;
