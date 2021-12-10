fb00b91a4c44a8be94c4aa93f6a4a04abef34057
42KIM <ksahoo524@gmail.com>
2021-12-10 22:34:56 +0900

    refactor: SM-84 Album 컴포넌트 스타일 수정

--- src/components/compound/Album/styled.ts
+++ src/components/compound/Album/styled.ts
@@ -11,12 +11,19 @@ const StyledContainer = styled.div<Required<Pick<AlbumProps, 'type'>>>`
   gap: 5px;
   background-color: ${COLOR.BASIC_WHITE};
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  margin: 10px 20px;
+  margin: 10px 10px;
   width: ${({ type }): string => ALBUM_ATTRIBUTES[type].width};
   height: ${({ type }): string => ALBUM_ATTRIBUTES[type].height};
   border: 2px solid ${COLOR.BASIC_WHITE};
   border-radius: ${({ type }): string =>
-    ALBUM_ATTRIBUTES[type].shape === 'circle' ? '50%' : '20%'};
+    ALBUM_ATTRIBUTES[type].shape === 'circle' ? '50%' : '20px'};
+
+  & > span {
+    max-width: 100px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 `;
 
 export { StyledContainer };
