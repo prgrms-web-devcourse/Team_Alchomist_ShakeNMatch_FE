4781bebf4c6b3923fd4aa185e9ad73bb72fa2428
42KIM <ksahoo524@gmail.com>
2021-12-12 02:39:32 +0900

    refactor: SM-94 자식 컴포넌트들 styled component로 extends하여 사용

--- src/components/domain/IngredientSelectModal/styled.ts
+++ src/components/domain/IngredientSelectModal/styled.ts
@@ -1,8 +1,34 @@
 import styled from '@emotion/styled';
+import TextButton from '@compound/TextButton';
+import TitleSectionContainer from '@domain/TitleSectionContainer';
+
+const StyledTitleSectionContainer = styled(TitleSectionContainer)`
+  padding-top: 20px;
+`;
+
+const StyledTextButton = styled(TextButton)`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+`;
 
 const StyledTabContentContainer = styled.div`
   position: relative;
   height: 100%;
 `;
 
-export { StyledTabContentContainer };
+const StyledTextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+  width: 720px;
+`;
+
+export {
+  StyledTitleSectionContainer,
+  StyledTextButton,
+  StyledTabContentContainer,
+  StyledTextContainer
+};
