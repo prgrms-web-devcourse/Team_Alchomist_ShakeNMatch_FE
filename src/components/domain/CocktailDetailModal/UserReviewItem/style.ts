387c67d8be57d431c8e6b0033bd17bc955bec008
SeungrokYoon <sonicrok93@gmail.com>
2021-12-14 18:20:52 +0900

    feat: SM-153 UserReviewItem 추가 및 칵테일상세모달 스타일 수정

---
+++ src/components/domain/CocktailDetailModal/UserReviewItem/style.ts
@@ -0,0 +1,13 @@
+import { COLOR } from '@constants';
+import styled from '@emotion/styled';
+
+const StyledReview = styled.div`
+  width: 337px;
+  height: 103px;
+  background: ${COLOR.BASIC_WHITE};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 7px;
+  margin: 9px;
+`;
+
+export default StyledReview;
