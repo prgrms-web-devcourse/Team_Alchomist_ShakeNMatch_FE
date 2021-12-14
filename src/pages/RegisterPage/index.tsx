37da8f3a937bfd28b0eb77b1d0bfb6e178bfae7a
Alang Kim <rlaangh77@naver.com>
2021-12-14 18:41:34 +0900

    feat: SM-155 회원가입 테스트

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,14 +1,48 @@
+import { request } from '@apis/config';
 import { StyledPageContainerWithBackground } from '@base/PageContainerWithBackground/styled';
 import BackButton from '@domain/BackButton';
 import RegisterModal from '@domain/RegisterModal';
+import type { IUser, IUserForm } from '@models';
 import type { ReactElement } from 'react';
 import { useNavigate } from 'react-router';
 
+// test email
+const TEST_EMAIL = 'rlaangh77@naver.com';
+
 const RegisterPage = (): ReactElement => {
   const navigate = useNavigate();
+
+  const postRegister = async (
+    values: Partial<IUser>
+  ): Promise<IUser | undefined> => {
+    console.log(values);
+    const data = await request.post<IUser, IUser>('/user/join', values);
+    if (data) {
+      return data;
+    }
+  };
+
+  const handleRegister = async (value: IUserForm): Promise<any> => {
+    console.log(value);
+    console.log(TEST_EMAIL);
+    console.log('posting!');
+    const { nickname, age, gender, mbti } = value;
+    request.get('user/nickname/alangGY').then((data) => {
+      console.log(data);
+    });
+    const data = await postRegister({
+      nickname,
+      age,
+      mbti,
+      isMan: gender === '남자' ? true : false,
+      email: TEST_EMAIL
+    });
+    console.log(data);
+  };
+
   return (
     <StyledPageContainerWithBackground>
-      <RegisterModal />
+      <RegisterModal onSubmit={handleRegister} />
       <BackButton
         color='NAVY'
         onClick={(): void => {
