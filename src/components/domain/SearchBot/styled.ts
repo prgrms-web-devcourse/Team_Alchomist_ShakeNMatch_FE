80607c2d96fbb9da3138831f83b6bcf80d13c06b
42KIM <ksahoo524@gmail.com>
2021-12-15 01:03:07 +0900

    feat: SM-154 Search 봇 기본 기능 구현

---
+++ src/components/domain/SearchBot/styled.ts
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+import { Input } from '@base';
+import type { StyledInputProps } from './types';
+
+const StyledContainer = styled.div`
+  position: absolute;
+  right: 30px;
+  bottom: 10px;
+`;
+
+const StyledBot = styled.div`
+  position: relative;
+  /* width: 50px;
+  height: 50px; */
+`;
+
+const StyledInputBackground = styled.div`
+  background-color: white;
+  position: absolute;
+  top: 20px;
+  left: -300px;
+  right: 0;
+  margin: 0 auto;
+  width: 200px;
+`;
+
+const StyledInput = styled(Input)<StyledInputProps>`
+  display: ${({ isVisible }): string => (isVisible ? 'block' : 'none')};
+`;
+
+export { StyledContainer, StyledBot, StyledInputBackground, StyledInput };
