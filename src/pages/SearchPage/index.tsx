b5f504a89e7184c3841f51e193010792cc76f666
42KIM <ksahoo524@gmail.com>
2021-12-17 06:14:58 +0900

    chore: SM-173 더미 데이터 삭제

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -7,17 +7,6 @@ import searchBartender from '@assets/characters/searchBartender.png';
 import SearchCocktailInput from '@domain/SearchCocktailInput';
 import { StyledContentWrapper } from './styled';
 import SectionDividerWithTitle from '@domain/SectionDividerWithTitle';
-// import { searchCocktailByName } from '@apis/cocktail';
-// import type { ICocktail } from '@models/types';
-
-// const DUMMY = [
-//   { id: '123', name: '칵테일', type: 'whiskey' },
-//   { id: '124', name: '칵테이', type: 'whiskey' },
-//   { id: '125', name: '칵테삼', type: 'whiskey' },
-//   { id: '126', name: '칵테사', type: 'whiskey' },
-//   { id: '127', name: '칵테오', type: 'whiskey' },
-//   { id: '128', name: '칵테육', type: 'whiskey' }
-// ];
 
 import useAxios from '@hooks/useAxios';
 import type { ICocktail, ICocktailSimple } from '@models/types';
@@ -33,7 +22,7 @@ const SearchPage = (): ReactElement => {
   // 이 페이지에서 ICocktail로 받아서 filteredResult로 바꿔서 내려주는 값을
   // 내일부터는 그냥 ICocktailSimple[]로 받으니까 바로 내려주면 된다.
   const searchCocktailByName = (keyword: string): Promise<ICocktail> => {
-    return request.get(`/cocktail/name?name=${keyword}`);
+    return request(`/cocktail/name?name=${keyword}`);
   };
 
   const handleSearch = useCallback((inputKeyword: string): void => {
