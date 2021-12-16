3594c72d18a6486040125f67b14082292373f794
Alang Kim <rlaangh77@naver.com>
2021-12-17 06:18:51 +0900

    refactor: SM-176 useAxios로 getCocktailListByTheme 로직 작성 및 리팩토링

--- src/pages/ThemePage/index.tsx
+++ src/pages/ThemePage/index.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Divider, SectionDivider, Text } from '@base';
+import { Loader } from '@compound';
+import { AXIOS_REQUEST_TYPE } from '@constants/axios';
 import { THEMES } from '@constants/themes';
 import BackButton from '@domain/BackButton';
 import CocktailList from '@domain/CocktailList';
 import ThemeSelector from '@domain/ThemeSelector';
+import useAxios from '@hooks/useAxios';
 import useDebounce from '@hooks/useDebounce';
-import type { ICocktail, ITHEME } from '@models';
+import type { ICocktail, ICocktailSimple, ITHEME } from '@models';
 import type { ReactElement } from 'react';
 import { useCallback, useMemo, useState, useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
@@ -16,24 +19,37 @@ import {
 } from './styled';
 
 const RADIX_TEN = 10;
-// 임시 상수
 const DEBOUNCE_DELAY = 700;
 
 const ThemePage = (): ReactElement => {
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
-  const selectedThemes = useMemo(
+  const request = useAxios(AXIOS_REQUEST_TYPE.DEFAULT);
+
+  const selectedThemesIndex = useMemo(
     () => ({
       main: searchParams.get('main') || '0',
       detail: searchParams.get('detail') || '0'
     }),
     [searchParams]
   );
+
+  const mainTheme = useMemo(
+    () =>
+      (Object.keys(THEMES) as ITHEME[])[
+        parseInt(selectedThemesIndex.main, RADIX_TEN)
+      ],
+    [selectedThemesIndex]
+  );
+  const detailTheme = useMemo(
+    () => THEMES[mainTheme][parseInt(selectedThemesIndex.detail, RADIX_TEN)],
+    [mainTheme, selectedThemesIndex]
+  );
   const showResult = useMemo(() => searchParams.get('result'), [searchParams]);
-  const [isMountedWithResult, setIsMountedWithResult] = useState(false); // 최초 접근 지점 확인 ( result 로 바로 접근인지 아닌지에 따라 handleBack navigate 지점 변경)
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // 최초 접근 지점 확인 ( result 로 바로 접근인지 아닌지에 따라 handleBack navigate 지점 변경)
+  const [isMountedWithResult, setIsMountedWithResult] = useState(false);
   const [cocktailList, setCocktailList] = useState<{
-    value: ICocktail[];
+    value: ICocktailSimple[];
     isLoading: boolean;
     error: any;
   }>({
@@ -42,103 +58,90 @@ const ThemePage = (): ReactElement => {
     error: null
   });
 
-  // 임시 cocktail get api 함수
-  // const getCocktailList = (main: string, detail: string): void => {
-  //   console.log(main, detail);
-  //   setCocktailList((prevCocktailList) => ({
-  //     ...prevCocktailList,
-  //     isLoading: true
-  //   }));
-  //   request
-  //     .get('https://jsonplaceholder.typicode.com/photos')
-  //     .then((data) => {
-  //       if (data && Array.isArray(data)) {
-  //         const newCocktailList = data
-  //           .slice(0, SLICE_COUNT)
-  //           .map(({ title, url }) => ({
-  //             name: title,
-  //             imageUrl: [url]
-  //           }));
-  //         console.log(newCocktailList);
-  //         setCocktailList({
-  //           value: newCocktailList as any[],
-  //           isLoading: false,
-  //           error: null
-  //         });
-  //       }
-  //     })
-  //     .catch((e) => {
-  //       console.error(e);
-  //       setCocktailList({
-  //         value: [],
-  //         isLoading: false,
-  //         error: 'error!'
-  //       });
-  //     });
-  // };
+  const getCocktailList = async (
+    main: string,
+    detail: string
+  ): Promise<any> => {
+    try {
+      setCocktailList((prevCocktailList) => ({
+        ...prevCocktailList,
+        isLoading: true
+      }));
+      const { data: cocktailList } = await request.get(
+        `/cocktail/theme?mainCategory=${main}&subCategory=${detail}`
+      );
+      if (cocktailList && Array.isArray(cocktailList)) {
+        setCocktailList({
+          value: cocktailList,
+          isLoading: false,
+          error: null
+        });
+      }
+    } catch (e) {
+      console.error(e);
+      setCocktailList({
+        value: [],
+        isLoading: false,
+        error: e
+      });
+    }
+  };
 
-  // const [debounceGetCocktailList] = useDebounce<[string, string]>(
-  //   (main, detail) => {
-  //     getCocktailList(main, detail);
-  //   },
-  //   DEBOUNCE_DELAY
-  // );
+  const [debounceGetCocktailList] = useDebounce<[string, string]>(
+    (main, detail) => {
+      getCocktailList(main, detail);
+    },
+    DEBOUNCE_DELAY
+  );
 
-  // const handleChangeTheme = useCallback(
-  //   ({ main, detail }: { main: number; detail: number }) => {
-  //     const newSearchParams: {
-  //       main: string;
-  //       detail: string;
-  //       result?: string;
-  //     } = {
-  //       main: main.toString(RADIX_TEN),
-  //       detail: detail.toString(RADIX_TEN)
-  //     };
-  //     const result = searchParams.get('result');
-  //     if (result === 'true') {
-  //       debounceGetCocktailList(
-  //         main.toString(RADIX_TEN),
-  //         detail.toString(RADIX_TEN)
-  //       );
-  //       newSearchParams.result = 'true';
-  //     }
-  //     setTimeout(() => {
-  //       // 동기 처리시 useEffect 내 첫렌더링에서 set이 진행되지 않음
-  //       setSearchParams(newSearchParams, { replace: true });
-  //     }, 0);
-  //   },
-  //   [searchParams]
-  // );
+  const handleChangeTheme = useCallback(
+    ({ main, detail }: { main: number; detail: number }) => {
+      const newSearchParams: {
+        main: string;
+        detail: string;
+        result?: string;
+      } = {
+        main: main.toString(RADIX_TEN),
+        detail: detail.toString(RADIX_TEN)
+      };
+      if (searchParams.get('result') === 'true') {
+        newSearchParams.result = 'true';
+      }
+      setTimeout(() => {
+        // 동기 처리시 useEffect 내 첫렌더링에서 set이 진행되지 않음
+        setSearchParams(newSearchParams, { replace: true });
+      }, 0);
+    },
+    [searchParams]
+  );
 
-  // const handleResult = useCallback(() => {
-  //   const { main, detail } = selectedThemes;
-  //   console.log(main, detail);
-  //   if (main !== null && detail !== null) {
-  //     setSearchParams({
-  //       main,
-  //       detail,
-  //       result: 'true'
-  //     });
-  //   }
-  //   getCocktailList(main, detail);
-  // }, [selectedThemes, setSearchParams]);
+  const handleResult = useCallback(() => {
+    const { main, detail } = selectedThemesIndex;
+    if (main !== null && detail !== null) {
+      setSearchParams({
+        main,
+        detail,
+        result: 'true'
+      });
+    }
+  }, [selectedThemesIndex, setSearchParams]);
 
   const handleBack = useCallback(() => {
     if (isMountedWithResult) {
-      const { main, detail } = selectedThemes;
+      const { main, detail } = selectedThemesIndex;
       setSearchParams({ main, detail }, { replace: true }); // result 페이지로 바로 접근됬을경우
     } else {
       navigate(-1); // result 이전에서 접근해 온경우
     }
-  }, [navigate, isMountedWithResult, selectedThemes, setSearchParams]);
+  }, [navigate, isMountedWithResult, selectedThemesIndex, setSearchParams]);
 
   useEffect(() => {
     if (showResult) {
-      // const { main, detail } = selectedThemes;
-      // getCocktailList(main, detail);
+      debounceGetCocktailList(mainTheme, detailTheme);
+
       setIsMountedWithResult(true);
     }
-  }, []);
+  }, [mainTheme, detailTheme, showResult]);
 
   return (
     <>
@@ -149,7 +152,9 @@ const ThemePage = (): ReactElement => {
               <Text>당신이 선택한 </Text>
               <Text bold color='BLUE' italic>
                 {`'${
-                  Object.keys(THEMES)[parseInt(selectedThemes.main)] as ITHEME
+                  Object.keys(THEMES)[
+                    parseInt(selectedThemesIndex.main)
+                  ] as ITHEME
                 }' `}
               </Text>
               <Text>에 어울리는 칵테일!</Text>
@@ -164,23 +169,20 @@ const ThemePage = (): ReactElement => {
           width='200vw'
         >
           <ThemeSelector
-            initialDetailIndex={parseInt(selectedThemes.detail || '0', 10)}
-            initialMainIndex={parseInt(selectedThemes.main || '0', 10)}
-            // onChangeIndex={handleChangeTheme}
+            initialDetailIndex={parseInt(selectedThemesIndex.detail || '0', 10)}
+            initialMainIndex={parseInt(selectedThemesIndex.main || '0', 10)}
+            onChangeIndex={handleChangeTheme}
           />
           <StyledResultContainer>
             {cocktailList.isLoading ? (
-              <Text>Loading</Text>
+              <Loader />
             ) : (
               <CocktailList cocktailList={cocktailList.value} />
             )}
           </StyledResultContainer>
         </SectionDivider>
         {!showResult ? (
-          <StyledResultButton
-            buttonType='LONG_WHITE'
-            // onClick={handleResult}
-          >
+          <StyledResultButton buttonType='LONG_WHITE' onClick={handleResult}>
             결과 보기
           </StyledResultButton>
         ) : (
