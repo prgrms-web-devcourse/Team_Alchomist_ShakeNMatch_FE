f26baceac67bd10a095dca9854b33771d4428cc3
Alang Kim <rlaangh77@naver.com>
2021-12-17 17:47:07 +0900

    refactor: SM-162 OAuthKaKaoPage 수정

--- src/pages/OAuthKaKaoPage/index.tsx
+++ src/pages/OAuthKaKaoPage/index.tsx
@@ -1,9 +1,11 @@
 // import { request } from '@apis/config';
 import { StyledPageContainerWithBackground } from '@base/PageContainerWithBackground/styled';
+import { Loader } from '@compound';
 import { useAuthorization } from '@contexts';
 import type { ReactElement } from 'react';
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { StyledLoaderContainer } from './styled';
 
 // const POST_AUTHORIZED_CODE_URL = '';
 
@@ -19,6 +21,7 @@ const OAuthKaKaoPage = (): ReactElement => {
     if (token) {
       setOAuthToken(token);
       setSearchParams({}, { replace: true });
+      console.log(needInfo);
       needInfo === 'true' ? navigate('/register') : navigate('/'); // 로그인시에는 별도
       // handlePostCode();
     } else {
@@ -28,7 +31,9 @@ const OAuthKaKaoPage = (): ReactElement => {
 
   return (
     <StyledPageContainerWithBackground>
-      잠시기다려주세요...
+      <StyledLoaderContainer>
+        <Loader />
+      </StyledLoaderContainer>
     </StyledPageContainerWithBackground>
   );
 };
