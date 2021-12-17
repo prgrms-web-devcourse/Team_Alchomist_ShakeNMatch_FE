0b6ad1eb17711e3cef36189fa760a4d3922f9001
42KIM <ksahoo524@gmail.com>
2021-12-17 20:56:02 +0900

    feat: SM-179 술장고 context 로직 추가

--- src/contexts/Jango/index.tsx
+++ src/contexts/Jango/index.tsx
@@ -1,7 +1,10 @@
 import type { ReactElement, ReactNode } from 'react';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import type { IJangoContext } from './types';
 import type { IIngredient } from '@models/types';
+import { useAuthorization } from '@contexts';
+import useAxios from '@hooks/useAxios';
+import { AXIOS_REQUEST_TYPE } from '@constants/axios';
 
 const JangoContext = createContext<IJangoContext | null>(null);
 const useJangoContext = (): IJangoContext => {
@@ -13,13 +16,46 @@ const useJangoContext = (): IJangoContext => {
 
 const JangoProvider = ({ children }: { children: ReactNode }): ReactElement => {
   const [userIngredients, setUserIngredients] = useState<IIngredient[]>([]);
+  const [totalIngredientsList, setTotalIngredientsList] = useState<{
+    [key: string]: IIngredient;
+  }>({});
+  console.log('hs', totalIngredientsList);
+  const { user } = useAuthorization();
+
+  useEffect(() => {
+    user?.ingredients && setUserIngredients(user.ingredients);
+  }, []);
+
+  const request = useAxios(AXIOS_REQUEST_TYPE.DEFAULT);
+  const getTotalIngredients = (): Promise<IIngredient[]> => {
+    return request.get('/ingredient');
+  };
+
+  useEffect(() => {
+    const setTotalIngredientList = async (): Promise<void> => {
+      const totalIngredients = await getTotalIngredients();
+      const totalIngredientList: {
+        [key: number]: IIngredient;
+      } = {};
+
+      totalIngredients.forEach((ingredient) => {
+        totalIngredientList[ingredient.id] = ingredient;
+      });
+
+      setTotalIngredientsList(totalIngredientList);
+    };
+
+    setTotalIngredientList();
+  }, []);
 
   const updateJangoContext = (recentIngredients: IIngredient[]): void => {
     setUserIngredients(recentIngredients);
   };
 
   return (
-    <JangoContext.Provider value={{ userIngredients, updateJangoContext }}>
+    <JangoContext.Provider
+      value={{ userIngredients, totalIngredientsList, updateJangoContext }}
+    >
       {children}
     </JangoContext.Provider>
   );
