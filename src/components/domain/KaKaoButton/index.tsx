32168728fb9362709b871ef02ccf1dadd21830a4
Alang Kim <rlaangh77@naver.com>
2021-12-19 03:34:03 +0900

    feat: SM-197 새창으로 로그인 기능

--- src/components/domain/KaKaoButton/index.tsx
+++ src/components/domain/KaKaoButton/index.tsx
@@ -3,6 +3,7 @@ import type { ReactElement } from 'react';
 import kakaoSrc from '@assets/oauthAssets/kakao_login.png';
 import { useCustomNavigate } from '@contexts/CustomNavigate';
 import type { KaKaoButtonProps } from './types';
+import { DOMAINS } from '@constants';
 // import { useNavigate } from 'react-router';
 // import { DOMAINS } from '@constants';
 const { REACT_APP_BASE_URL: BASE_URL } = process.env;
@@ -12,13 +13,19 @@ const { REACT_APP_BASE_URL: BASE_URL } = process.env;
 // const REDIRECT_URI = `${BASE_URL}/login`;
 
 const KaKaoButton = ({ redirectUrl }: KaKaoButtonProps): ReactElement => {
-  const { savePath } = useCustomNavigate();
+  const { savePath, navigate } = useCustomNavigate();
   const handleLogin = (): void => {
     // window.location.assign(
     //   `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&state=alchomist`
     // );
     redirectUrl && savePath(redirectUrl);
-    window.location.assign(`${BASE_URL}/login`);
+    window.open(
+      `${BASE_URL}/login`,
+      '__blank',
+      'fullscreen=no, width=500, height=600, resizable=no'
+    );
+    navigate(`/${DOMAINS.oauthKaKao}?loading=true`);
+    // window.location.assign(`${BASE_URL}/login`);
   };
 
   return (
