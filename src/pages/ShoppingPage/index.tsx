c3fc935d4485b2372f53e5410f3e865ae99a383c
sshyukk <sanghyuk0328@gmail.com>
2021-12-19 18:14:48 +0900

    feat: SM-199 팝업 창 위치 가운데 고

--- src/pages/ShoppingPage/index.tsx
+++ src/pages/ShoppingPage/index.tsx
@@ -7,8 +7,6 @@ import { useParams } from 'react-router-dom';
 
 const ShoppingPage = (): ReactElement => {
   const params = useParams();
-  console.log('keyword: ', params, params.keyword);
-
   const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -19,6 +17,7 @@ const ShoppingPage = (): ReactElement => {
       setError(null);
       setData([]);
       setLoading(true);
+      // 무한 스크롤 리펙토링 해보기.
       const response = await axios.get('/v1/search/shop.json?', {
         params: {
           query: params.keyword,
@@ -42,11 +41,29 @@ const ShoppingPage = (): ReactElement => {
     fetchData();
   }, []);
 
-  if (loading) return <div>로딩중</div>;
+  if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
   if (!data) return <div>데이터를 불러오지 못했습니다.</div>;
 
-  // API 연동한 후에 리스트로 뿌려주기.
+  // 팝업 생성 함수.
+  const openPopup = (
+    popupWidth: number,
+    popupHeight: number,
+    popupUrl: string
+  ): void => {
+    const top = (window.innerHeight - popupHeight) / 2 + screenY;
+    const left = (window.innerWidth - popupWidth) / 2 + screenX;
+    let spec = 'status=no, menubar=no, toolbar=no, resizable=no';
+    spec += ', width=' + popupWidth + ', height=' + popupHeight;
+    spec += ', top=' + top + ', left=' + left;
+    window.open(popupUrl, 'popup', spec);
+  };
+
+  // 팝업 창 크기 조절.
+  const WINDOW_WIDTH_SIZE = 1000;
+  const WINDOW_HEIGHT_SIZE = 500;
+
+  // API 연동한 부분 리스트로 뿌려주기.
   const shoppingItemList = Children.toArray(
     data.map((shoppingItem) => (
       <ShoppingItem
@@ -54,41 +71,16 @@ const ShoppingPage = (): ReactElement => {
         price={shoppingItem.lprice}
         title={shoppingItem.title}
         vendor={shoppingItem.mallName}
-        // 리펙토링 할 것.
+        // ShoppingItem 컴포넌트에서 리펙토링 해보기.
         onClick={(): void => {
-          window.open(
-            shoppingItem.link,
-            '_blank',
-            'fullscreen=no, width=1000, height=500, centerscreen=yes, left=100, top=100'
-          );
+          openPopup(WINDOW_WIDTH_SIZE, WINDOW_HEIGHT_SIZE, shoppingItem.link);
         }}
-      ></ShoppingItem>
+      />
     ))
   );
 
-  // 팝업을 띄워주는 함수.
-  // const openPopup = (
-  //   popupWidth: number,
-  //   popupHeight: number,
-  //   popupUrl: string
-  // ): void => {
-  //   const width = popupWidth;
-  //   const height = popupHeight;
-  //   const top = (window.innerHeight - height) / 2 + screenY;
-  //   const left = (window.innerWidth - width) / 2 + screenX;
-
-  //   let spec = 'status=no, menubar=no, toolbar=no, resizable=no';
-  //   spec += ', width=' + width + ', height=' + height;
-  //   spec += ', top=' + top + ', left' + left;
-
-  //   window.open(popupUrl, 'popup', spec);
-  // };
-
   return (
     <>
-      {/* <button type='button' onClick={fetchData}>
-        네이버 검색 API 불러오기
-      </button> */}
       <StyledContainer>
         <TitleSectionContainer
           alignItems={true}
