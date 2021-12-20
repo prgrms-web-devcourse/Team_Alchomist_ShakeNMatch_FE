8e32543ef097fa8539751404716c5aa1f5640470
Alang Kim <rlaangh77@naver.com>
2021-12-21 03:09:09 +0900

    refactor: SM-211 ThemeSelector 테마별 색상 상수화 및 Carousel Style 변경

--- src/components/compound/Carousel/CarouselContainer/styled.ts
+++ src/components/compound/Carousel/CarouselContainer/styled.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 const StyledCarouselContainer = styled.div`
   position: relative;
+  cursor: ns-resize;
 `;
 
 export { StyledCarouselContainer };
--- src/utils/constants/themes.ts
+++ src/utils/constants/themes.ts
@@ -1,3 +1,5 @@
+import type { IThemesColor } from '@models';
+
 const THEME_MOOD = ['HAPPY', 'ANGRY', 'SAD', 'ROMANTIC', 'LONELY'] as const;
 const THEME_WEATHER = ['SUNNY', 'RAINY', 'CLOUDY', 'SNOWY'] as const;
 const THEME_COLOR = [
@@ -47,4 +49,72 @@ const THEMES = {
   // TRENDING: THEME_TRENDING
 } as const;
 
-export { THEMES };
+const THEMES_COLOR: IThemesColor = {
+  MOOD: {
+    main: 'BASIC_WHITE',
+    detail: {
+      HAPPY: 'ORANGE',
+      ANGRY: 'STRONG_PINK',
+      SAD: 'BLUE',
+      ROMANTIC: 'LIGHT_PINK',
+      LONELY: 'DARK_GRAY'
+    }
+  },
+  WEATHER: {
+    main: 'BASIC_WHITE',
+    detail: {
+      SUNNY: 'ORANGE',
+      RAINY: 'BLUE',
+      CLOUDY: 'DARK_GRAY',
+      SNOWY: 'BRIGHT_IVORY'
+    }
+  },
+  COLOR: {
+    main: 'BASIC_WHITE',
+    detail: {
+      RED: 'STRONG_PINK',
+      ORANGE: 'ORANGE',
+      YELLOW: 'LIGHT_YELLOW',
+      GREEN: 'GREEN',
+      BLUE: 'BLUE',
+      PURPLE: 'VIOLET',
+      WHITE: 'BASIC_WHITE',
+      BLACK: 'BLACK_OPACITY'
+    }
+  },
+  MUSIC: {
+    main: 'BASIC_WHITE',
+
+    detail: {
+      JAZZ: 'DARK_GRAY',
+      ROCK: 'STRONG_PINK',
+      BALLAD: 'LIGHT_PINK',
+      HIPHOP: 'BLUE',
+      CLASSIC: 'BRIGHT_BROWN',
+      INDIE: 'GREEN'
+    }
+  },
+  MBTI: {
+    main: 'BASIC_WHITE',
+    detail: {
+      INTJ: 'LIGHT_PINK',
+      INFP: 'LIGHT_PINK',
+      INTP: 'LIGHT_PINK',
+      ENTJ: 'LIGHT_PINK',
+      ENTP: 'LIGHT_PINK',
+      INFJ: 'LIGHT_PINK',
+      ENFJ: 'LIGHT_PINK',
+      ENFP: 'LIGHT_PINK',
+      ISTJ: 'LIGHT_PINK',
+      ISFJ: 'LIGHT_PINK',
+      ESTJ: 'LIGHT_PINK',
+      ESFJ: 'LIGHT_PINK',
+      ISTP: 'LIGHT_PINK',
+      ISFP: 'LIGHT_PINK',
+      ESTP: 'LIGHT_PINK',
+      ESFP: 'LIGHT_PINK'
+    }
+  }
+} as const;
+
+export { THEMES, THEMES_COLOR };
