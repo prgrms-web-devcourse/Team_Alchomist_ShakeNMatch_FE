e8331e989dbd857d89a79b7ee75dca1476eae364
Alang Kim <rlaangh77@naver.com>
2021-12-21 20:00:30 +0900

    refactor: SM-211 SectionDivider 반응형 추가

--- src/components/base/SectionDivider/styled.ts
+++ src/components/base/SectionDivider/styled.ts
@@ -1,8 +1,11 @@
 import styled from '@emotion/styled';
 import type { CSSObject } from '@emotion/react';
 import type { StyledSectionContainerProps } from './types';
+import { HEADER_TEMPLATE_MARGIN } from '@constants/margin';
 
-const StyledSectionContainer = styled.div<StyledSectionContainerProps>`
+const StyledSectionContainer = styled.div<
+  Required<StyledSectionContainerProps>
+>`
   display: inline-flex;
   overflow: hidden;
   flex-direction: ${({ direction }): string | boolean =>
@@ -18,6 +21,17 @@ const StyledSectionContainer = styled.div<StyledSectionContainerProps>`
   ${({ rotate }): CSSObject => ({
     transform: `rotate(${rotate})`
   })};
+
+  @media screen and (max-width: 1500px) {
+    width: ${({ width }): string | number =>
+      typeof width === 'string' ? width : `${width}px`};
+  }
+  @media screen and (min-width: 1500px) {
+    width: ${({ width }): string | number =>
+      typeof width === 'string'
+        ? `calc(${width} - ${HEADER_TEMPLATE_MARGIN}px)`
+        : `${width - HEADER_TEMPLATE_MARGIN}px`};
+  }
 
   & > * {
     transition: all 0.7s ease-in-out;
